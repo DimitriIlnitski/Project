86beb66c634ad25f494fd6c43dae8ede845c3ed4
DimitriIlnitski <47006346+DimitriIlnitski@users.noreply.github.com>
2021-12-27 22:06:12 +0200

    SC

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,6 +4,11 @@ const productController = require('../controllers/productController.js')
 const productRouter = express.Router()
 
 productRouter.use('/postproduct', productController.postProduct)
+productRouter.use('/editProduct', productController.editProduct)
+productRouter.use('/removeproduct', productController.removeProduct)
+productRouter.use('/sortProduct', productController.sortProduct)
+productRouter.use('/searchProduct', productController.searchProduct)
+productRouter.use('/getVolume', productController.getVolume)
 productRouter.use('/create', productController.addProduct)
 productRouter.use('/', productController.getProducts)
 
